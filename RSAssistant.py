4e39ceb3a2ab17b6ce9181a94fc63e7f1acf7ff4
braydio <93299297+braydio@users.noreply.github.com>
2025-05-01 08:44:10 -0400

    1/35: Add on_ready, process_sell_list, show_order_queue and related top-level logic

--- RSAssistant.py
+++ RSAssistant.py
@@ -1,37 +1,49 @@
+# RSAssistant.py
 
-############################################
-# F21: watched_ticker - Removes a ticker from the watchlist.
+# Standard library imports
+import asyncio
+import json
+import logging
+import os
+import signal
+import sys
+from datetime import datetime, timedelta
 
-#############################################
-# F23: chunk
-messages - Chunks long messages to fit Windows lints.
+# Third-party imports
+import discord
+from discord import Embed
+import discord.ext as lext
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 
-#############################################
-# F24: print_by_line - Sends text file to Discord, line-by-line.
-
-############################################
-# F25: add_account_mappings_command - Adds account details to mapping json.
-
-#############################################
-# F26: load_account_mappings_command - Loads mappings from the excel sheet.
-
-#############################################
-# F27: update_log_with_mappings - Updates excel log with mappings.
-
-#############################################
-# F28: clear_mapping_command - Clears all account mappings.
-
-##############################################
-# F29: clear_holdings - Clears entries in holdings log.
-
-############################################
-# F32: send_negative_holdings - Sends alerts for negative quantity.
-
-############################################
-# F33: shutdown - Shots down the bot and stops schedules.
-
-#############################################
-# F34: shutdown_handler - Shutdown handler for SYSRQL.
-
-#############################################
-# F35: __main__ execution block - Starts the bot with token.
+# Local application imports
+from utils.config_utils import (
+    ACCOUNT_MAPPING,
+    BOT_TOKEN,
+    DISCORD_PRIMARY_CHANNEL,
+    DISCORD_SECONDARY_CHANNEL,
+    EXCEL_FILE_MAIN,
+    HOLDINGS_LOG_CSV,
+    VERSION,
+i
+rom utils.csv_utils import clear_holdings_log, remove_from_holdings_embed, sell_all_position,
+from utils.excel_utils import (
+    add_account_mappings,
+    clear_account_mappings,
+    index_account_details,
+    map_accounts_in_excel_log,
+)
+from utils.order_exec import process_sell_list, schedule_and_execute
+from utils.parsing_utils import (
+    alert_channel_message,
+    parse_embed_message,
+    parse_order_message,
+)
+from utils.autobuy_utils import autobuy_ticker
+from utils.order_queue_manager import (
+    add_to_order_queue,
+    get_order_queue,
+    remove_order,
+    list_order_queue,
+)
+from utils.sql_utils import bot_query_database, get_db_connection, init_db
