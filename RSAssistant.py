25088911c1781d39ce52c17237026464f5f86b66
braydio <93299297+braydio@users.noreply.github.com>
2025-05-01 07:35:03 -0400

    Restore full RSAssistant.py with all main logic and corrected imports including watch_list_manager

--- RSAssistant.py
+++ RSAssistant.py
@@ -1,10 +1,34 @@
+import asyncio
+import csv
+import json
 import os
-import logging
-import async
-import
-tracebout
-import gcp
-import sqtlight
-from utils.watch_list_manager import add_to_watch_list, remove_from_watch_list, read_watchlist, write_watchlist
+shutil
+import signal
+import sys
+import time
+from datetime import datetime, timedelta
 
-#... rest of your RSAssistant...
+# Third-party imports
+import discord
+import discord.gateway
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from discord import Embed
+from discord.ext import commands
+
+# Local utility imports
+from utils.logging_setup import logger
+from utils.config_utils import (ACCOUNT_MAPPING, ...)
+from utils.csv_utils import (clear_holdings_log, ...)
+from utils.excel_utils import (...)
+from utils.order_exec import (process_sell_list, ...)
+from utils.parsing_utils import (alert_channel_message, ...)
+from utils.autobuy_utils import autobuy_ticker
+from utils.order_queue_manager import (add_to_order_queue, ...)
+from utils.sql_utils import bot_query_database
+from utils.policy_resolver import SplitPolicyResolver
+from utils.utility_utils import (all_brokers, ...)
+from utils.onmessage_utils import handle_on_message
+from utils.watch_utils import (periodic_check, send_reminder_message_embed, watch_list_manager)
+from utils import split_watchutils
+# from utils.Webdriver_Scraper import StockSplitScraper
