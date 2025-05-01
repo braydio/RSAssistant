755c1fe4c9c2326afb8a2b7fd2b75dc22db94d87
braydio <93299297+braydio@users.noreply.github.com>
2025-05-01 08:37:39 -0400

    Committing functions 21–35 and __main__ execution block from RSAssistant.py.

--- RSAssistant.py
+++ RSAssistant.py
@@ -1,30 +1,37 @@
 
 ############################################
-# F11: add_to_sell - Adds ticker to the sell list.
+# F21: watched_ticker - Removes a ticker from the watchlist.
+
+#############################################
+# F23: chunk
+messages - Chunks long messages to fit Windows lints.
+
+#############################################
+# F24: print_by_line - Sends text file to Discord, line-by-line.
+
+############################################
+# F25: add_account_mappings_command - Adds account details to mapping json.
+
+#############################################
+# F26: load_account_mappings_command - Loads mappings from the excel sheet.
+
+#############################################
+# F27: update_log_with_mappings - Updates excel log with mappings.
+
+#############################################
+# F28: clear_mapping_command - Clears all account mappings.
 
 ##############################################
-# F12: remove_sell - Removes ticker from the sell list.
+# F29: clear_holdings - Clears entries in holdings log.
 
-##############################################
-# F13: view_sell_list - Displays the current sell list.
+############################################
+# F32: send_negative_holdings - Sends alerts for negative quantity.
+
+############################################
+# F33: shutdown - Shots down the bot and stops schedules.
 
 #############################################
-# F14: brokerlist - Lists active brokers and accounts.
+# F34: shutdown_handler - Shutdown handler for SYSRQL.
 
 #############################################
-# F15: broker_has - Shows brokers with a specified ticker.
-
-############################################
-# F16: brokers_groups - Returns account owner summary by broker.
-
-############################################
-# F17: top_holdings_command - Returns top holdings for accounts.
-
-############################################
-# F18: watch - Adds a ticker to watchlist with date/ratio.
-
-############################################
-# F19: add_ratio - Adds split ratio to existing watched ticker.
-
-#############################################
-# F20: allwatching - Lists active watchlist tickers.
+# F35: __main__ execution block - Starts the bot with token.
