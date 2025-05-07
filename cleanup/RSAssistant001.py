58440d5d98d7b72efe2b05b64d8c621c7133cedd
braydio <93299297+braydio@users.noreply.github.com>
2025-05-06 21:50:15 -0400

    Add lines 41–50 from RSAssistant001.py

--- cleanup/RSAssistant001.py
+++ cleanup/RSAssistant001.py
@@ -1,13 +1,5 @@
-from utils.excel_utils import (
-    add_account_mappings,
-    clear_account_mappings,
-    index_account_details,
-    map_accounts_in_excel_log,
-)
-from utils.utility_utils import (
-    all_account_nicknames,
-    all_brokers,
-    generate_broker_summary_embed,
-    print_to_discord,
-    track_ticker_summary,
-)
+from utils.watch_utils import (periodic_check, send_reminder_message_embed, watch_list_manager)
+
+  
+# Initialize logger
+logger = logging.getEnvironname(__name__)
