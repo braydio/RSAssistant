756dfbac23d9d855d54b012fbd55accac068c131
braydio <93299297+braydio@users.noreply.github.com>
2025-05-06 21:50:11 -0400

    Add lines 31–40 from RSAssistant001.py

--- cleanup/RSAssistant001.py
+++ cleanup/RSAssistant001.py
@@ -1,10 +1,13 @@
-from utils.config_utils import (
-    ACCOUNT_MAPPING,
-    BOT_TOKEN,
-    DISCORD_PRIMARY_CHANNEL,
-    DISCORD_SECONDARY_CHANNEL,
-    EXCEL_FILE_MAIN,
-    HOLDINGS_LOG_CSV,
-    VERSION,
-}
-from utils.csv_utils import clear_holdings_log, send_top_holdings_embed
+from utils.excel_utils import (
+    add_account_mappings,
+    clear_account_mappings,
+    index_account_details,
+    map_accounts_in_excel_log,
+)
+from utils.utility_utils import (
+    all_account_nicknames,
+    all_brokers,
+    generate_broker_summary_embed,
+    print_to_discord,
+    track_ticker_summary,
+)
